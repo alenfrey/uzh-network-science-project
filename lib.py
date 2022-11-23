66980c34a5c912f9cadb0eed9e3dcda723c7713f
af <datenmarmelade@gmail.com>
2022-11-23 09:25:18 +0100

    initial commit

---
+++ lib.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# PATHS AND FILES
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+PROJECT_ROOT_DIR_PATH = Path(__file__).parent.absolute() # project root dir path
+
+DATA_DIR_PATH = PROJECT_ROOT_DIR_PATH / "data" # data dir path
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# FUNCTIONS
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
---
+++ notes/notes.md
@@ -0,0 +1,5 @@
+* pathcensus uses igraph library for creating graph objects, I might port this to networkx
+* code to reproduce paper is available at https://github.com/sztal/scs-paper
+  the best way to install the dependencies is via conda/miniconda since it depends
+  on graph-tool and igraph.
+
